07615b93ce676bfaac09a2a23a84c21fe5174989
Matus <matiasfrancalancia123321@gmail.com>
2025-10-13 08:36:20 -0300

    Generar proyecto desde Sprint Initializr

---
+++ src/test/java/com/mz/bibliteca_api/BiblitecaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mz.bibliteca_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblitecaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
