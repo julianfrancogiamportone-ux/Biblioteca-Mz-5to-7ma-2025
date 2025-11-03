da0bbe2f8fae11626d6082f5e1229a32df8c61ed
davidubillalobos <davidubillalobos@gmail.com>
2025-11-03 11:42:46 +0000

    Actualizacion de entidad Profesor

---
+++ src/main/java/com/mz/bibliteca_api/iservice/IProfesorService.java
@@ -0,0 +1,15 @@
+ackage com.mz.bibliteca_api.iservice;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.mz.bibliteca_api.entity.Profesor;
+
+public interface IProfesorService {
+    public List<Profesor> findAllprofesor();
+    public Profesor saveProfesor(profesor Profesor);
+	public Optional<Profesor> findprofesorById(Long id);
+	public void deleteProfesor(Long id);
+	public void deleteProfesor(profesor Profesor);
+	public void deleteAllprofesor();
+}
