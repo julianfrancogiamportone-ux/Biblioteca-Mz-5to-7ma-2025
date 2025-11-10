c8aeea253f9494a3f3b3ac51005f98e0ab8e13ce
Matus <matiasfrancalancia123321@gmail.com>
2025-11-10 13:06:00 -0300

    Solucionar problemas con los imports y packages

--- src/main/java/com/mz/bibliteca_api/iservice/IAlumnoService.java
+++ src/main/java/com/mz/bibliteca_api/iservice/IAlumnoService.java
@@ -1,15 +1,15 @@
-ackage com.mz.bibliteca_api.iservice;
+package com.mz.bibliteca_api.iservice;
 
 import java.util.List;
 import java.util.Optional;
 
-import com.mz.bibliteca_api.entity.Alumnos;
+import com.mz.bibliteca_api.entity.Alumno;
 
 public interface IAlumnoService {
-    public List<Alumnos> findAllAuthors();
-    public Alumnos saveAlumnos(alumnos Alumnos);
-	public Optional<Alumnos> findAlumnosById(Long id);
+    public List<Alumno> findAllAlumnos();
+    public Alumno saveAlumnos(Alumno Alumnos);
+	public Optional<Alumno> findAlumnosById(Long id);
 	public void deleteAlumnos(Long id);
-	public void deleteAlumnos (alumnos Alumnos);
+	public void deleteAlumnos (Alumno Alumnos);
 	public void deleteAllAlumnos();
 }
--- src/main/java/com/mz/bibliteca_api/repository/AlumnoRepository.java
+++ src/main/java/com/mz/bibliteca_api/repository/AlumnoRepository.java
@@ -1,2 +1,9 @@
+package com.mz.bibliteca_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mz.bibliteca_api.entity.Alumno;
+
 @Repository
-interface AlumnoRepository extends JpaRepository<Alumno, Long> {}
+public interface AlumnoRepository extends JpaRepository<Alumno, Long> {}
